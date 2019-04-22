ee7e0eb7cc868d4d773f027fbcc6e248d74917da
”Ozi <31310132+OziOcb@users.noreply.github.com>
2019-04-22 22:10:55 +0100

    Add ability to search and display images

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -2,6 +2,7 @@
   <input
     id="search"
     name="search"
+    :class="{ dark }"
     :value="value"
     @input="handleChange"
   />
@@ -15,6 +16,10 @@ export default {
       type: String,
       required: true,
     },
+    dark: {
+      type: Boolean,
+      default: false,
+    }
   },
   methods: {
     handleChange(e) {
@@ -47,6 +52,15 @@ export default {
     &:focus {
       outline: none;
       box-shadow: 0 10px 20px -8px rgba(255,255,255,.5);
+    }
+  }
+
+  .dark {
+    color: #1e3d4a;
+    border-bottom-color: #1e3d4a;
+
+    &:focus {
+      box-shadow: 0 10px 20px -8px rgba(#1e3d4a, .2);
     }
   }
 </style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="wrapper">
-    <HeroImage />
-    <Claim />
-    <SearchInput v-model="searchValue" @input="handleInput"/>
+  <div :class="[{flexStart: step === 1}, 'wrapper']">
+    <transition name="slide">
+      <img class="logo" src="https://img.icons8.com/color/48/000000/nasa.png" alt="logo" v-if="step === 1">
+    </transition>
+    <transition name="fade">
+      <HeroImage v-if="step === 0" />
+    </transition>
+    <Claim v-if="step === 0" />
+    <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1" />
+    <div class="results" v-if="results && !loading && step === 1">
+      <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" />
+    </div>
   </div>
 </template>
 
@@ -12,6 +20,7 @@ import debounce from 'lodash.debounce';
 import Claim from '@/components/Claim.vue';
 import SearchInput from '@/components/SearchInput.vue';
 import HeroImage from '@/components/HeroImage.vue';
+import Item from '@/components/Item.vue';
 
 const API = 'https://images-api.nasa.gov/search';
 
@@ -21,19 +30,25 @@ export default {
     Claim,
     SearchInput,
     HeroImage,
+    Item,
   },
   data() {
     return {
+      loading: false,
+      step: 0,
       searchValue: '',
       results: [],
     }
   },
   methods: {
     handleInput: debounce(function() {
+      this.loading = true;
       console.log(this.searchValue);
       axios.get(`${API}?q=${this.searchValue}&media_type=image`)
         .then((response) => {
           this.results = response.data.collection.items;
+          this.loading = false;
+          this.step = 1;
         })
         .catch((error) => {
           console.log(error);
@@ -44,6 +59,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .3s ease;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+  .slide-enter-active, .slide-leave-active {
+    transition: margin-top .3s ease;
+  }
+  .slide-enter, .slide-leave-to {
+    margin-top: -50px;
+  }
+
   .wrapper {
     display: flex;
     flex-direction: column;
@@ -53,5 +82,15 @@ export default {
     padding: 30px;
     width: 100%;
     min-height: 100vh;
+    position: relative;
+
+    &.flexStart {
+      justify-content: flex-start;
+    }
+  }
+
+  .logo {
+    position: absolute;
+    top: 10px;
   }
 </style>
